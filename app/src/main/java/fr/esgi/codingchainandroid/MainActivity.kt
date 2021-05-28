e4ade945cfc6c642a50c0f97fb412cf353bf84c5
Louis Dumont <louisdumontbarbosa@gmail.com>
2021-05-28 17:17:01 +0200

    design: implemented login page

--- app/src/main/java/fr/esgi/codingchainandroid/MainActivity.kt
+++ app/src/main/java/fr/esgi/codingchainandroid/MainActivity.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.connection_activity)
+        setContentView(R.layout.login_activity)
     }
 }
