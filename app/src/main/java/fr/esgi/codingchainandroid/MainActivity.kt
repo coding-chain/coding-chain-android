eef21dc65d738d5d011d2d3f71c6f229be5b5639
Dumont Louis <louisdumontbarbosa@gmail.com>
2021-05-29 11:00:04 +0200

    design: fixed up connection_activity.xml

--- app/src/main/java/fr/esgi/codingchainandroid/MainActivity.kt
+++ app/src/main/java/fr/esgi/codingchainandroid/MainActivity.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.register_activity)
+        setContentView(R.layout.connection_activity)
     }
 }
