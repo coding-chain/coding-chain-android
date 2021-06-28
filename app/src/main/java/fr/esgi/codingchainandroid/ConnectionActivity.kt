c58bae185411530e25760a24400e8947ccd0887f
LAymeric <aymeric.larvet@gmail.com>
2021-06-28 18:11:17 +0200

    design and navigation between connection or home

---
+++ app/src/main/java/fr/esgi/codingchainandroid/ConnectionActivity.kt
@@ -0,0 +1,14 @@
+package fr.esgi.codingchainandroid
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ConnectionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.connection_activity);
+
+    }
+
+}
--- app/src/main/java/fr/esgi/codingchainandroid/MainActivity.kt
+++ app/src/main/java/fr/esgi/codingchainandroid/MainActivity.kt
@@ -1,11 +1,27 @@
 package fr.esgi.codingchainandroid
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.connection_activity)
+
+        setContentView(R.layout.main_activity);
+
+        if(isConnected()){
+            val intent = Intent(this, HomeActivity::class.java);
+            startActivity(intent)
+        }else{
+            val intent = Intent(this, ConnectionActivity::class.java);
+            startActivity(intent)
+        }
+    }
+
+    private fun isConnected(): Boolean {
+        // TODO check if has a connexion token
+        return false;
     }
 }
