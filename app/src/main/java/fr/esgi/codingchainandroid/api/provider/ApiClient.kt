263b1a6150c17b6bfa3d5e90750fb7fcde189a4d
Louis Dumont <louisdumontbarbosa@gmail.com>
2021-07-29 11:07:38 +0200

    add prod url to api

--- app/src/main/java/fr/esgi/codingchainandroid/api/provider/ApiClient.kt
+++ app/src/main/java/fr/esgi/codingchainandroid/api/provider/ApiClient.kt
@@ -13,7 +13,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 
 object ApiClient {
-    private const val BASE_URL: String = "http://10.0.2.2:5002/api/v1/"
+    private const val BASE_URL: String = "http://20.76.221.209/api/api/v1/"
     private val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
     private val networkInterceptor = NetworkInterceptor()
 
